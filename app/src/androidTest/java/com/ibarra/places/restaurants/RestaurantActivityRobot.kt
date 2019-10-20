b8ce30fb6ab5d15af36af24aaae31c52c03bbc0e
joaoibarra <joao.ibarra@gmail.com>
2019-10-20 13:31:56 -0300

    Create simple instrumental tests

---
+++ app/src/androidTest/java/com/ibarra/places/restaurants/RestaurantActivityRobot.kt
@@ -0,0 +1,75 @@
+package com.ibarra.places.restaurants
+
+import android.content.Intent
+import androidx.test.rule.ActivityTestRule
+import br.com.concretesolutions.kappuccino.assertions.VisibilityAssertions
+import br.com.concretesolutions.kappuccino.custom.recyclerView.RecyclerViewInteractions
+import com.ibarra.places.R
+import com.ibarra.places.extensions.loadResponse
+import com.ibarra.places.ui.restaurants.RestaurantListActivity
+import okhttp3.mockwebserver.Dispatcher
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.MockWebServer
+import okhttp3.mockwebserver.RecordedRequest
+
+fun arrange(rule: ActivityTestRule<RestaurantListActivity>, func: RestaurantListActivityArrangeRobot.() -> Unit) = RestaurantListActivityArrangeRobot(
+    rule
+).apply { func() }
+fun act(func: RestaurantListActivityActRobot.() -> Unit) = RestaurantListActivityActRobot().apply { func() }
+fun assert(func: RestaurantListActivityAssertRobot.() -> Unit) = RestaurantListActivityAssertRobot().apply { func() }
+
+class RestaurantListActivityArrangeRobot(private val rule: ActivityTestRule<RestaurantListActivity>) {
+
+    private val sourceResponse by lazy { "places.json".loadResponse() }
+
+    fun launchActivity() {
+        rule.launchActivity(Intent())
+    }
+
+    fun mockWebServer(mockWebServer: MockWebServer) {
+        val dispatcher = object : Dispatcher() {
+            @Throws(InterruptedException::class)
+            override fun dispatch(request: RecordedRequest): MockResponse {
+                return when {
+                    request.path.orEmpty().contains("sources") -> {
+                        MockResponse().setResponseCode(200).setBody(sourceResponse)
+                    }
+                    else -> MockResponse().setResponseCode(404)
+                }
+            }
+        }
+
+        mockWebServer.dispatcher = dispatcher
+    }
+}
+
+class RestaurantListActivityActRobot {
+    fun clickPlaceItemByPosition(position: Int) {
+        RecyclerViewInteractions.recyclerView(R.id.rv_restaurants) {
+            atPosition(position) {
+                click()
+            }
+        }
+    }
+}
+
+class RestaurantListActivityAssertRobot {
+    fun isRestaurantListVisible() {
+        VisibilityAssertions.displayed {
+            id(R.id.rv_restaurants)
+        }
+    }
+
+    fun verifyItemByPosition(position: Int = 0) {
+        RecyclerViewInteractions.recyclerView(R.id.rv_restaurants) {
+            atPosition(position) {
+                displayed {
+                    id(R.id.restaurant_name)
+                    id(R.id.restaurant_rating)
+                    id(R.id.restaurant_address)
+                    id(R.id.restaurant_cuisines)
+                }
+            }
+        }
+    }
+}
