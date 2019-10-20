ed27b6dc8ce2b41bfa616117d179b590f3a4a613
joaoibarra <joao.ibarra@gmail.com>
2019-10-20 10:19:24 -0300

    Start Koin inside de Application

---
+++ app/src/main/java/com/ibarra/places/IbarraPlacesApp.kt
@@ -0,0 +1,19 @@
+package com.ibarra.places
+
+import android.app.Application
+import com.ibarra.places.di.NetworkModule
+import com.ibarra.places.di.RoomModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+
+class IbarraPlacesApp : Application(){
+
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidContext(this@IbarraPlacesApp)
+            modules(listOf(NetworkModule, RoomModule))
+        }
+
+    }
+}
