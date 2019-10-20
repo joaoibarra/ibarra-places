c0dd3332ff52437e35ff6aa3686694992c8b65e5
joaoibarra <joao.ibarra@gmail.com>
2019-10-20 13:17:50 -0300

    Create simple Dao Test

---
+++ app/src/test/java/com/ibarra/places/di/RoomTestModule.kt
@@ -0,0 +1,16 @@
+package com.ibarra.places.di
+
+import androidx.room.Room
+import com.ibarra.places.data.db.AppDatabase
+import org.koin.android.ext.koin.androidContext
+import org.koin.dsl.module
+
+val RoomTestModule = module {
+    single {
+        // In-Memory database config
+        Room.inMemoryDatabaseBuilder(androidContext(), AppDatabase::class.java)
+            .allowMainThreadQueries()
+            .build()
+    }
+    single { get<AppDatabase>().getRestaurantDao() }
+}
